1e23f1ab3d51650ce67d49a1e9d3409da8204c67
kingsley-amoke <klordbravo@gmail.com>
2024-05-20 02:14:51 +0100

    added bio to user schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -30,6 +30,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String,},
+    bio: String,
     role: { type: String,},
     address: { type: String,},
     location: String,
