e021bda70f91a880b90abaada6047e823b55acb1
kingsley-amoke <klordbravo@gmail.com>
2024-06-08 22:46:25 +0100

    added wallet and referral

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,49 +1,53 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const jobSchema = require('./jobSchema')
-const notificationSchema = require('./notificationSchema')
+const jobSchema = require("./jobSchema");
+const notificationSchema = require("./notificationSchema");
 
 const GuarantorSchema = new mongoose.Schema({
-    name: {
-        type: 'String', required: true
-    },
-    phone: {type: 'String', required: true}
-        
-})
+  name: {
+    type: "String",
+    required: true,
+  },
+  phone: { type: "String", required: true },
+});
 
 const skillsSchema = new mongoose.Schema({
-    name: {
-        type: 'String', required: true
-    },
-    level: {
-        type: 'String', required: true
-    }
-})
+  name: {
+    type: "String",
+    required: true,
+  },
+  level: {
+    type: "String",
+    required: true,
+  },
+});
 
 const statusSchema = new mongoose.Schema({
-    isVerified: Boolean,
-    isVIP: Boolean
-})
-
+  isVerified: Boolean,
+  isVIP: Boolean,
+});
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String,},
-    bio: {type: String},
-    role: { type: String,},
-    address: { type: String,},
-    location: String,
-    nin: { type: String,},
-    image: { type: String },
-    phone: { type: String, },
-    status: statusSchema,
-    gender: String,
-    guarantors: [GuarantorSchema],
-    jobs: [jobSchema],
-    skills: [String],
-    notifications: [notificationSchema]
-})
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String },
+  bio: { type: String },
+  role: { type: String },
+  address: { type: String },
+  location: String,
+  nin: { type: String },
+  image: { type: String },
+  phone: { type: String },
+  walletBalance: { type: String, default: "0" },
+  referralBalance: { type: String, default: "0" },
+  referalls: [String],
+  status: statusSchema,
+  gender: String,
+  guarantors: [GuarantorSchema],
+  jobs: [jobSchema],
+  skills: [String],
+  notifications: [notificationSchema],
+});
 
-module.exports = userSchema
+module.exports = userSchema;
